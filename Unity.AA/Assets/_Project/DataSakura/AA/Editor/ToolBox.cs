a4fd7ed500742bacd82dcce998ead67b779b3de4
vangogih <vangogih@yandex.ru>
2023-06-09 13:29:29 +0200

    aligned shoot btn with joystick

---
+++ Unity.AA/Assets/_Project/DataSakura/AA/Editor/ToolBox.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+
+namespace _Project.DataSakura.AA.Editor
+{
+    public class ToolBox
+    {
+        [MenuItem("DataSakura/Scenes/Bootstrap &1", priority = 202)]
+        public static void OpenBootstrapScene()
+        {
+            EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+            EditorSceneManager.OpenScene("Assets/_Project/Scenes/0.Bootstrap.unity");
+        }
+
+        [MenuItem("DataSakura/Scenes/Battle &2", priority = 202)]
+        public static void OpenCoreScene()
+        {
+            EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+            EditorSceneManager.OpenScene("Assets/_Project/Scenes/1.Battle.unity");
+        }
+    }
+}
